af9de23f031d7b1a33163b5d8cf86edb52823062
Elijah Jacobsen <39361743+CarbonGhost@users.noreply.github.com>
2024-10-11 13:28:19 -0600

    new: Initial template

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import type { Component } from "solid-js";
+
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+
+const App: Component = () => {
+	return (
+		<div>
+			<h2 style={{ "margin-top": 0, "margin-bottom": 0 }}>necromancy.info</h2>
+			<span>a site by carbon</span>
+			<br />
+			<span>I swear I'll put something here soon</span>
+		</div>
+	);
+};
+
+export default App;
